7a6df4108a1f73422d511848bbfd36595aed0357
Pietro Lusso <p.lusso@verband.creditreform.de>
2018-07-11 15:20:23 +0200

    Initial Commit Maven Project

---
+++ src/main/java/net/hydrogen2oxygen/markdowntohomepage/MarkdowntohomepageApplication.java
@@ -0,0 +1,12 @@
+package net.hydrogen2oxygen.markdowntohomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdowntohomepageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdowntohomepageApplication.class, args);
+    }
+}
---
+++ src/test/java/net/hydrogen2oxygen/markdowntohomepage/MarkdowntohomepageApplicationTests.java
@@ -0,0 +1,16 @@
+package net.hydrogen2oxygen.markdowntohomepage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarkdowntohomepageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
