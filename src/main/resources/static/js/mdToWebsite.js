d9bf09638cef0b10e601944c21b4604e9e2732f7
hydrogen2oxygen <luxuspeter@googlemail.com>
2018-08-23 16:13:54 +0200

    Show new project in selectbox after saving

--- src/main/resources/static/js/mdToWebsite.js
+++ src/main/resources/static/js/mdToWebsite.js
@@ -8,14 +8,22 @@ md.init = function () {
     $('#buttonCreateNewWebsite').click(md.createNewWebsite);
 
     // Init selectBox projects
-    $.getJSON("website", function (data) {
-        $.each(data, function () {
-            $("#selectProjects").append('<option value="' + this.name + '">' + this.name + '</option>')
-        });
-    });
+    md.reloadWebsitesSelectBox();
 
     console.log('End of init');
     console.log('========================');
+};
+
+md.reloadWebsitesSelectBox = function () {
+
+    $("#selectProjects").html("<option selected>Choose a project ...</option>");
+
+    $.getJSON("website", function (data) {
+
+        $.each(data, function () {
+            $("#selectProjects").append('<option value="' + this.name + '">' + this.name + '</option>');
+        });
+    });
 };
 
 md.createNewWebsite = function () {
@@ -30,11 +38,30 @@ md.createNewWebsite = function () {
         async: true,
         success: function (response) {
             console.log(response);
+            md.reloadWebsitesSelectBox();
+            $("#createNewWebsiteForm").trigger("reset");
+            $(".collapse").collapse('hide');
+            md.message("success","Project saved!");
         },
         dataType: "json"
     });
 
     return false;
+};
+
+md.message = function (messageType, messageText) {
+    $('#messageDialog').html('<span class="badge badge-' + messageType +'">' + messageText + '</span>');
+    $('#messageDialog').css('visibility', 'visible');
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    setTimeout(function () {
+        $("#messageDialog").animate({
+            opacity: 0.25,
+            left: "+=50",
+            height: "toggle"
+        }, 3000, function () {
+            $('#messageDialog').css('visibility', 'hidden');
+        });
+    },5000);
 };
 
 /**
